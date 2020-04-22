3d88a4006757a3720341716359a0a658f671ca1e
Sam Broyles <s.broyles@students.clark.edu>
2020-04-22 16:19:32 -0700

    Changed header info. added art and new phrases.

---
+++ ASCII art.py
@@ -0,0 +1,27 @@
+"""
+CTEC 121
+Sam Broyles
+Programming Assignment
+Programming Assignment ASCII Art
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print()
+    print("Welcome To Jurassic Park")
+    print()
+    print("               __")
+    print("              / _)")
+    print("     _.----._/ /")
+    print("    /         /")
+    print(" __/ (  | (  |")
+    print("/__.-'|_|--|_|")
+    print()
+    print("Thanks For Visiting")
+    print()
+main()
---
+++ Greetings.py
@@ -0,0 +1,19 @@
+"""
+CTEC 121
+Sam Broyles
+Programming Assignment
+Programming Assignment ASCII Art
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print("GREETINGS HUMAN")
+    print("I AM A HUMAN AS WELL")
+    print("EVEN THOUGH I LOOK LIKE A COMPUTER I ASSURE YOU I AM HUMAN")
+    print()
+main()
